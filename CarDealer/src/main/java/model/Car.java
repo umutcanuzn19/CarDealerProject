6a99c4de9bc34af4d3c602ef3dbdf6272bf1e445
tasso <tasso@DESKTOP-G1J74BO>
2020-05-24 20:10:37 +0300

    Table and column update

---
+++ CarDealer/src/main/java/model/Car.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.util.ArrayList;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Dealer")
+public class Car {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id ;
+	
+	@Column(name = "car_brand")
+	private ArrayList<String> carBrand ; 
+	@Column(name = "car_model")
+	private String carModel ;
+	@Column(name = "car_code")
+	private String carCode ;
+	@Column(name="car_price")
+	private float carPrice;
+	@Column(name="car_case")
+	private ArrayList<String> carCase ;
+	
+	@JoinColumn(name="Dealer")
+	private Dealer dealerId ;
+	
+}
---
+++ CarDealer/src/main/java/model/Dealer.java
@@ -0,0 +1,23 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "Dealer")
+public class Dealer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id ;
+	
+	@Column(name="f_name")
+	private String fname ;
+	@Column(name="l_name")
+	private String lname;
+	@Column(name="p_number")
+	private int phoneNumber ;
+}
